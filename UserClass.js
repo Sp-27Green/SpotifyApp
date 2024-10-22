a8fac1941dcb0cd6027669ce385cfa2122541e81
Marc Gagnon <chewie83@gmail.com>
2024-10-22 14:04:52 -0400

    Quick update on TemplateListItem format.

--- UserClass.js
+++ UserClass.js
@@ -1,4 +1,6 @@
 //Used to hold the current user's info. Looking for a possible better way of doing this since its unencrypted. 
+import {TempoInterval} from './TempoIntervalClass'
+import {TempoIntervalTemplate} from './TempoIntervalTemplateClass'
 
 export class User{
     constructor(){
@@ -10,6 +12,7 @@ export class User{
         this.currentPlayer = "";
         this.usersIntervalTemplates = [];
         this.tempoInterval  = []
+        this.setTestTemplate()
     
     }
 
@@ -60,5 +63,31 @@ export class User{
 
     setCurrentPlayer(currentPlayer){
         this.currentPlayer = currentPlayer;
+    }
+
+    setTestTemplate(){
+        if(this.usersIntervalTemplates.length == 0){  
+            var intervalOne = new TempoInterval()
+            intervalOne.setIntervalType("increase");
+            intervalOne.setSongAmount(3)
+            intervalOne.setLowTempo(110)
+            intervalOne.setHighTempo(140)
+            var intervalTwo = new TempoInterval()
+            intervalTwo.setIntervalType("random");
+            intervalTwo.setSongAmount(10)
+            intervalTwo.setLowTempo(140)
+            intervalTwo.setHighTempo(140)
+            var intervalThree = new TempoInterval()
+            intervalThree.setIntervalType("decrease")
+            intervalThree.setSongAmount(7)
+            intervalThree.setLowTempo(125)
+            intervalThree.setHighTempo(140)
+            var template = new TempoIntervalTemplate("Test template")
+            template.intervalArray.push(intervalOne)
+            template.intervalArray.push(intervalTwo)
+            template.intervalArray.push(intervalThree)
+            this.usersIntervalTemplates.push(template) 
+        }                          
+        
     }
 }
